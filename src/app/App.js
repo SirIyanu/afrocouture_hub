42b73f0126a213714c49e6343fe325661e1efc31
Delphine Fanwi <njabehdelphine0@gmail.com>
2024-04-15 21:30:06 +0300

    My first version for homepage

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import { LoginPage } from "./components/login/login";
+import Heading from "./components/login/heading";
+import SecondSection from "./components/login/secondsection";
+import FirstPicAndClothes from "./components/login/firstpicandclothes";
+import SecondLineOfPics from "./components/login/secondlineofpics";
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <Heading/>
+      <SecondSection/>
+      <FirstPicAndClothes/>
+      <SecondLineOfPics/>
     </div>
   );
 }
