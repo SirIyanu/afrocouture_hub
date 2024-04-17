e10c10f4f3f49122fdda5624e7d620c505e52c51
DamilolaOmotoyinbo <damilolaomotoyinbo@gmail.com>
2024-04-17 16:17:58 +0100

    work

--- src/app/pages/Landing/index.jsx
+++ src/app/pages/Landing/index.jsx
@@ -2,10 +2,13 @@ import Head from "../../components/Shop/Head";
 import Hero from "../../components/Shop/Hero";
 import Side from "../../components/Shop/Side";
 import Body from "../../components/Shop/Body";
+// import style from "./style.module.css";
+
+import "./components/Shop/Shop.css";
+
 export const LandingPage = () => {
   return (
     <div>
-      {/* <LoginPage /> */}
       <div>
         <div>
           <Head />
