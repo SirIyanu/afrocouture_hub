cd467ddef26a99ce82d84f7202b3ebbec3dbf953
naapire <vidanaapire@gmail.com>
2024-04-17 20:12:54 +0000

    resolve conflict

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
-import { LoginPage } from "./components/login/login";
-import NavigationPage from "./components/login/Header/homePage";
-import Footerbar from "./components/login/Footer/footer";
-import Success from "./components/login/success/Success";
-import Error from "./components/login/error/error";
-
-=======
 import { LoginPage } from "./pages/Login";
 import { SignUpPage } from "./pages/SignUp";
 import { LandingPage } from "./pages/Landing";
@@ -16,7 +8,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserPage } from "./pages/UserPage";
 import React from "react";
->>>>>>> 5cc331c0745cf581b9ea607e9105bfe1b7857f92
 
 import "./components/Shop/Shop.css";
 
@@ -48,19 +39,9 @@ const router = createBrowserRouter([
 ]);
 export function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      {/* <LoginPage /> */}
-      <NavigationPage />
-      <Footerbar/>
-      {/* <Success/> */}
-      {/* <Error/> */}
-    </div>
-=======
     <>
       <ToastContainer />
       <RouterProvider router={router} />
     </>
->>>>>>> 5cc331c0745cf581b9ea607e9105bfe1b7857f92
   );
 }
