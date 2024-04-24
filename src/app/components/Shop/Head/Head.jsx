258d75344e98ecc23d261f2799396593c5e9697a
DamilolaOmotoyinbo <damilolaomotoyinbo@gmail.com>
2024-04-24 18:46:39 +0100

    work

--- src/app/components/Shop/Head/Head.jsx
+++ src/app/components/Shop/Head/Head.jsx
@@ -4,9 +4,8 @@ import React from "react";
 import logo from "../../Assets/logo.svg";
 import lens from "../../Assets/lens.png";
 import user from "../../Assets/user.png";
-// import user from "../../Assets/user.png";
-// import favorite from "../../Assets/favorite.png";
-// import cart from "../../Assets/cart.png";
+import favorite from "../../Assets/favorite.png";
+import cart from "../../Assets/cart.png";
 import style from "./head.module.css";
 
 export const Head = () => {
@@ -19,8 +18,8 @@ export const Head = () => {
       </button>
       <ul className={style.navlist}>
         <li><img src={user} className={style.user} alt="" /></li>
-        {/* <li><img src={favorite} className={style.favorite} alt="" /></li>
-        <li><img src={cart} className={style.cart} alt="" /></li> */}
+        <li><img src={favorite} className={style.favorite} alt="" /></li>
+        <li><img src={cart} className={style.cart} alt="" /></li>
         <li className={style.bestseller}>
           <a href="bestseller">Become a best seller</a>
         </li>
