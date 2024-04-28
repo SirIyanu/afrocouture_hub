bf7c73d31a93665130d95b6e483687333c935676
Delphine Fanwi <njabehdelphine0@gmail.com>
2024-04-28 12:10:52 +0300

    final homepage

--- src/app/components/Homepage/photogrid/photogrid.jsx
+++ src/app/components/Homepage/photogrid/photogrid.jsx
@@ -1,17 +1,5 @@
 import style from "./photogrid.module.css";
-import Pica from "../../../assets/afro pictures/TOP PICKS FOR YOU 1.jpg";
-import Picb from "../../../assets/afro pictures/TOP PICKS FOR YOU 2.jpg";
-import Picc from "../../../assets/afro pictures/TOP PICKS FOR YOU 3.jpg";
-import Picd from "../../../assets/afro pictures/TOP PICKS FOR YOU 4.jpg";
-import Pice from "../../../assets/afro pictures/TOP PICKS FOR YOU 5.jpg";
-import Pic_1 from "../../../assets/afro pictures/luxury girl 1.jpg";
-import Pic2 from "../../../assets/afro pictures/Luxury girl 2.jpg";
-import Pic3 from "../../../assets/afro pictures/luxury girl 3.jpg";
-import Pic4 from "../../../assets/afro pictures/luxury girl 4.jpg";
-import Picf from "../../../assets/afro pictures/hero section 1.jpg";
-import pic5 from "../../../assets/afro pictures/hero section 1.jpg";
-import pic6 from "../../../assets/afro pictures/Hero section 2.jpg";
-import pic7 from "../../../assets/afro pictures/TOP PICKS FOR YOU 4.jpg";
+
 import pic8 from "../../../assets/afro pictures/overlay and images (1).jpg";
 
 
